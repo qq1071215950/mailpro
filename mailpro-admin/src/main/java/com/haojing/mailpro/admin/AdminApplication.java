f051088953608a93ceb9fc603f0c59381b5aa683
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-10 17:59:12 +0800

    admin模块配置pom初始化

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.haojing.mailpro.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiange
+ * @date 2020/6/10 17:49
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
