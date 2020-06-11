0739de1844245c4f1b46bb0a1141dcca28c12eca
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-11 10:02:49 +0800

    暂时不做处理admin的oss坐标问题

---
+++ mailpro-portal/src/main/java/com/haojing/mailpro/portal/PortalApplication.java
@@ -0,0 +1,15 @@
+package com.haojing.mailpro.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiange
+ * @date 2020/6/10 20:01
+ */
+@SpringBootApplication
+public class PortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApplication.class, args);
+    }
+}
