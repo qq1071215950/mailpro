dc84159bc6eda771f4aa2b4c0538caed69a26ecf
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-19 17:37:57 +0800

    后台菜单管理controller定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsMenuController.java
@@ -0,0 +1,16 @@
+package com.haojing.mailpro.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 后台菜单管理
+ * @author jiange
+ * @date 2020/6/19 17:37
+ */
+@RestController
+@Api(tags = "UmsMenuController", description = "后台菜单管理")
+@RequestMapping("/menu")
+public class UmsMenuController {
+}
