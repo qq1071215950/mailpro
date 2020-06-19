afe4c30b0f17ef9d5a71554055c40bdf87ea3843
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-19 17:51:39 +0800

    后台权限管理的controller定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsPermissionController.java
@@ -0,0 +1,16 @@
+package com.haojing.mailpro.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 后台权限管理controller
+ * @author jiange
+ * @date 2020/6/19 17:50
+ */
+@RestController
+@Api(tags = "UmsPermissionController", description = "后台用户权限管理")
+@RequestMapping("/permission")
+public class UmsPermissionController {
+}
