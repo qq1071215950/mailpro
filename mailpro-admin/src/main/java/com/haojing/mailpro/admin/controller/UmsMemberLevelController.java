a2e0fbea8b360412c493aecf21a1400b4ad080e3
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-19 17:19:42 +0800

    会员等级管理的controller定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsMemberLevelController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 会员等级管理Controller
+ * @author jiange
+ * @date 2020/6/19 17:18
+ */
+@RestController
+@Api(tags = "UmsMemberLevelController", description = "会员等级管理")
+@RequestMapping("/memberLevel")
+public class UmsMemberLevelController {
+
+}
