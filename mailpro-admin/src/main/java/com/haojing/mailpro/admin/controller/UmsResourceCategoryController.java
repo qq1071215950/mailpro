bde618e9523ff18feb9cbcefe7e135a3a9886f44
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-19 18:46:32 +0800

    后台资源管理controller定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsResourceCategoryController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 后台资源分类管理Controller
+ * @author jiange
+ * @date 2020/6/19 18:45
+ */
+@RestController
+@Api(tags = "UmsResourceCategoryController", description = "后台资源分类管理")
+@RequestMapping("/resourceCategory")
+public class UmsResourceCategoryController {
+
+}
