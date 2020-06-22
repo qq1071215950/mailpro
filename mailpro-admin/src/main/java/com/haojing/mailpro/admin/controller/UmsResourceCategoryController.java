52a4b08bac9c67b32d44762154d09cb85d929e3c
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-22 19:23:18 +0800

    后台资源管理定义

--- mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsResourceCategoryController.java
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsResourceCategoryController.java
@@ -25,7 +25,6 @@ public class UmsResourceCategoryController {
 
     @ApiOperation("查询所有后台资源分类")
     @RequestMapping(value = "/listAll", method = RequestMethod.GET)
-    @ResponseBody
     public CommonResult<List<UmsResourceCategory>> listAll() {
         List<UmsResourceCategory> resourceList = resourceCategoryService.listAll();
         return CommonResult.success(resourceList);
@@ -33,7 +32,6 @@ public class UmsResourceCategoryController {
 
     @ApiOperation("添加后台资源分类")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    @ResponseBody
     public CommonResult create(@RequestBody UmsResourceCategory umsResourceCategory) {
         int count = resourceCategoryService.create(umsResourceCategory);
         if (count > 0) {
@@ -45,7 +43,6 @@ public class UmsResourceCategoryController {
 
     @ApiOperation("修改后台资源分类")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
-    @ResponseBody
     public CommonResult update(@PathVariable Long id,
                                @RequestBody UmsResourceCategory umsResourceCategory) {
         int count = resourceCategoryService.update(id, umsResourceCategory);
@@ -58,7 +55,6 @@ public class UmsResourceCategoryController {
 
     @ApiOperation("根据ID删除后台资源")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    @ResponseBody
     public CommonResult delete(@PathVariable Long id) {
         int count = resourceCategoryService.delete(id);
         if (count > 0) {
---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/UmsResourceController.java
@@ -0,0 +1,18 @@
+package com.haojing.mailpro.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *后台资源管理Controller
+ * @author jiange
+ * @date 2020/6/22 19:22
+ */
+@RestController
+@Api(tags = "UmsResourceController", description = "后台资源管理")
+@RequestMapping("/resource")
+public class UmsResourceController {
+
+}
