72fa580d5b619c4d0f42a64f105c107944814b1b
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 10:04:54 +0800

    后台品牌的定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/pms/PmsBrandController.java
@@ -0,0 +1,16 @@
+package com.haojing.mailpro.admin.controller.pms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 品牌功能Controller
+ * @author jiange
+ * @date 2020/6/23 10:04
+ */
+@RestController
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
+@RequestMapping("/brand")
+public class PmsBrandController {
+}
