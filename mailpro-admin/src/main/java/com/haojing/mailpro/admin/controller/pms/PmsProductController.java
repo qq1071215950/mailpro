bfc47410401d45258bdc36f9deff28281dea8660
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 11:12:05 +0800

    后台商品管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/pms/PmsProductController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.pms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品管理
+ * @author jiange
+ * @date 2020/6/23 11:11
+ */
+@RestController
+@Api(tags = "PmsProductController", description = "商品管理")
+@RequestMapping("/product")
+public class PmsProductController {
+
+}
