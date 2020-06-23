bb00a2869e83762daf6f7b5c12ac5506b98f8249
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 14:45:29 +0800

    后台商品属性管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/pms/PmsProductAttributeController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.pms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品属性管理
+ * @author jiange
+ * @date 2020/6/23 14:44
+ */
+@RestController
+@Api(tags = "PmsProductAttributeController", description = "商品属性管理")
+@RequestMapping("/productAttribute")
+public class PmsProductAttributeController {
+
+}
