2d23f154016e0e5143568b43de4da312faca6ffa
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 10:19:35 +0800

    后台品牌的实现 1 获取全部品牌 2 新增 3 修改 4 删除 5 批量操作处理

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,111 @@
+package com.haojing.mailpro.admin.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.haojing.mailpro.admin.dto.PmsBrandParam;
+import com.haojing.mailpro.admin.service.PmsBrandService;
+import com.haojing.mailpro.mbg.mapper.PmsBrandMapper;
+import com.haojing.mailpro.mbg.mapper.PmsProductMapper;
+import com.haojing.mailpro.mbg.model.PmsBrand;
+import com.haojing.mailpro.mbg.model.PmsBrandExample;
+import com.haojing.mailpro.mbg.model.PmsProduct;
+import com.haojing.mailpro.mbg.model.PmsProductExample;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * 商品品牌管理
+ * @author jiange
+ * @date 2020/6/23 10:10
+ */
+@Service
+public class PmsBrandServiceImpl implements PmsBrandService {
+    @Autowired
+    private PmsBrandMapper brandMapper;
+    @Autowired
+    private PmsProductMapper productMapper;
+
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int createBrand(PmsBrandParam pmsBrandParam) {
+        PmsBrand pmsBrand = new PmsBrand();
+        BeanUtils.copyProperties(pmsBrandParam, pmsBrand);
+        //如果创建时首字母为空，取名称的第一个为首字母
+        if (StringUtils.isEmpty(pmsBrand.getFirstLetter())) {
+            pmsBrand.setFirstLetter(pmsBrand.getName().substring(0, 1));
+        }
+        return brandMapper.insertSelective(pmsBrand);
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrandParam pmsBrandParam) {
+        PmsBrand pmsBrand = new PmsBrand();
+        BeanUtils.copyProperties(pmsBrandParam, pmsBrand);
+        pmsBrand.setId(id);
+        //如果创建时首字母为空，取名称的第一个为首字母
+        if (StringUtils.isEmpty(pmsBrand.getFirstLetter())) {
+            pmsBrand.setFirstLetter(pmsBrand.getName().substring(0, 1));
+        }
+        //更新品牌时要更新商品中的品牌名称
+        PmsProduct product = new PmsProduct();
+        product.setBrandName(pmsBrand.getName());
+        PmsProductExample example = new PmsProductExample();
+        example.createCriteria().andBrandIdEqualTo(id);
+        productMapper.updateByExampleSelective(product,example);
+        return brandMapper.updateByPrimaryKeySelective(pmsBrand);
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return brandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public int deleteBrand(List<Long> ids) {
+        PmsBrandExample pmsBrandExample = new PmsBrandExample();
+        pmsBrandExample.createCriteria().andIdIn(ids);
+        return brandMapper.deleteByExample(pmsBrandExample);
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(String keyword, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        PmsBrandExample pmsBrandExample = new PmsBrandExample();
+        pmsBrandExample.setOrderByClause("sort desc");
+        PmsBrandExample.Criteria criteria = pmsBrandExample.createCriteria();
+        if (!StringUtils.isEmpty(keyword)) {
+            criteria.andNameLike("%" + keyword + "%");
+        }
+        return brandMapper.selectByExample(pmsBrandExample);
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public int updateShowStatus(List<Long> ids, Integer showStatus) {
+        PmsBrand pmsBrand = new PmsBrand();
+        pmsBrand.setShowStatus(showStatus);
+        PmsBrandExample pmsBrandExample = new PmsBrandExample();
+        pmsBrandExample.createCriteria().andIdIn(ids);
+        return brandMapper.updateByExampleSelective(pmsBrand, pmsBrandExample);
+    }
+
+    @Override
+    public int updateFactoryStatus(List<Long> ids, Integer factoryStatus) {
+        PmsBrand pmsBrand = new PmsBrand();
+        pmsBrand.setFactoryStatus(factoryStatus);
+        PmsBrandExample pmsBrandExample = new PmsBrandExample();
+        pmsBrandExample.createCriteria().andIdIn(ids);
+        return brandMapper.updateByExampleSelective(pmsBrand, pmsBrandExample);
+    }
+}
