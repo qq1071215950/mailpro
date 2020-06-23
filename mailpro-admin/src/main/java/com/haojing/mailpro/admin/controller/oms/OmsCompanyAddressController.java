671c410c2a3f4917ecb719d7e838298865cd68c9
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 16:00:50 +0800

    订单收货地址管理

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/oms/OmsCompanyAddressController.java
@@ -0,0 +1,18 @@
+package com.haojing.mailpro.admin.controller.oms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 收货地址管理
+ * @author jiange
+ * @date 2020/6/23 15:58
+ */
+@RestController
+@Api(tags = "OmsCompanyAddressController", description = "收货地址管理")
+@RequestMapping("/companyAddress")
+public class OmsCompanyAddressController {
+
+
+}
