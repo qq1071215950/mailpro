97941bc4e796050ff56694652e1af2111dfea04e
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 17:00:03 +0800

    退货申请管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/oms/OmsOrderReturnApplyController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.oms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单退货申请管理
+ * @author jiange
+ * @date 2020/6/23 16:58
+ */
+@RestController
+@Api(tags = "OmsOrderReturnApplyController", description = "订单退货申请管理")
+@RequestMapping("/returnApply")
+public class OmsOrderReturnApplyController {
+
+}
