bdafa2e734984d97adee5c0161c862ebaa746ff3
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 15:37:48 +0800

    mini文档对象上传

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/mini/MinioController.java
@@ -0,0 +1,82 @@
+package com.haojing.mailpro.admin.controller.mini;
+
+import com.haojing.mailpro.admin.dto.MinioUploadDto;
+import com.haojing.mailpro.common.api.CommonResult;
+import io.minio.MinioClient;
+import io.minio.policy.PolicyType;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author jiange
+ * @date 2020/6/23 15:28
+ */
+@Api(tags = "MinioController", description = "MinIO对象存储管理")
+@RequestMapping("/minio")
+@RestController
+public class MinioController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MinioController.class);
+    @Value("${minio.endpoint}")
+    private String ENDPOINT;
+    @Value("${minio.bucketName}")
+    private String BUCKET_NAME;
+    @Value("${minio.accessKey}")
+    private String ACCESS_KEY;
+    @Value("${minio.secretKey}")
+    private String SECRET_KEY;
+
+    @ApiOperation("文件上传")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public CommonResult upload(@RequestParam("file") MultipartFile file) {
+        try {
+            //创建一个MinIO的Java客户端
+            MinioClient minioClient = new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
+            boolean isExist = minioClient.bucketExists(BUCKET_NAME);
+            if (isExist) {
+                LOGGER.info("存储桶已经存在！");
+            } else {
+                //创建存储桶并设置只读权限
+                minioClient.makeBucket(BUCKET_NAME);
+                minioClient.setBucketPolicy(BUCKET_NAME, "*.*", PolicyType.READ_ONLY);
+            }
+            String filename = file.getOriginalFilename();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+            // 设置存储对象名称
+            String objectName = sdf.format(new Date()) + "/" + filename;
+            // 使用putObject上传一个文件到存储桶中
+            minioClient.putObject(BUCKET_NAME, objectName, file.getInputStream(), file.getContentType());
+            LOGGER.info("文件上传成功!");
+            MinioUploadDto minioUploadDto = new MinioUploadDto();
+            minioUploadDto.setName(filename);
+            minioUploadDto.setUrl(ENDPOINT + "/" + BUCKET_NAME + "/" + objectName);
+            return CommonResult.success(minioUploadDto);
+        } catch (Exception e) {
+            LOGGER.info("上传发生错误: {}！", e.getMessage());
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("文件删除")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult delete(@RequestParam("objectName") String objectName) {
+        try {
+            MinioClient minioClient = new MinioClient(ENDPOINT, ACCESS_KEY, SECRET_KEY);
+            minioClient.removeObject(BUCKET_NAME, objectName);
+            return CommonResult.success(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return CommonResult.failed();
+    }
+
+
+}
