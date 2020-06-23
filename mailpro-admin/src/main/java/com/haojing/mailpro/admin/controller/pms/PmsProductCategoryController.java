fb994a19c536f7d13816ecf475fa53c7c56b39b2
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 10:22:59 +0800

    后台商品分类定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/pms/PmsProductCategoryController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.pms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品分类
+ * @author jiange
+ * @date 2020/6/23 10:21
+ */
+@RestController
+@Api(tags = "PmsProductCategoryController", description = "商品分类管理")
+@RequestMapping("/productCategory")
+public class PmsProductCategoryController {
+
+}
