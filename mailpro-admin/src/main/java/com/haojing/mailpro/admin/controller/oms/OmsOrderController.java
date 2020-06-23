322db8ad2ebc4f1fa2fa09cfcfa728bc18794f93
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 16:07:01 +0800

    订单管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/oms/OmsOrderController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.oms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单管理
+ * @author jiange
+ * @date 2020/6/23 16:06
+ */
+@RestController
+@Api(tags = "OmsOrderController", description = "订单管理")
+@RequestMapping("/order")
+public class OmsOrderController {
+
+}
