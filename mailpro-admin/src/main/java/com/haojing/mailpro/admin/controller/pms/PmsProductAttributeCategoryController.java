03905d9f887f18cd961a4de64115512f87bc4957
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 15:16:17 +0800

    商品分类属性的定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/pms/PmsProductAttributeCategoryController.java
@@ -0,0 +1,17 @@
+package com.haojing.mailpro.admin.controller.pms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 商品属性分类Controller
+ * @author jiange
+ * @date 2020/6/23 15:15
+ */
+@RestController
+@Api(tags = "PmsProductAttributeCategoryController", description = "商品属性分类管理")
+@RequestMapping("/productAttribute/category")
+public class PmsProductAttributeCategoryController {
+
+}
