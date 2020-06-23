58d549774e6099e80a7785c7c0bcf0d7886446cb
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-23 17:19:43 +0800

    订单设置管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/oms/OmsOrderSettingController.java
@@ -0,0 +1,16 @@
+package com.haojing.mailpro.admin.controller.oms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单设置管理
+ * @author jiange
+ * @date 2020/6/23 17:18
+ */
+@Api(tags = "OmsOrderSettingController", description = "订单设置管理")
+@RequestMapping("/orderSetting")
+@RestController
+public class OmsOrderSettingController {
+}
