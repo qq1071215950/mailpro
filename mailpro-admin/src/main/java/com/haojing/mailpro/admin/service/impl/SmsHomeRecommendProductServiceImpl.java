a70f1a867ef1ca72b50cecc1d518e615d29c850a
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-24 10:33:57 +0800

    人气推荐管理

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/service/impl/SmsHomeRecommendProductServiceImpl.java
@@ -0,0 +1,70 @@
+package com.haojing.mailpro.admin.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.haojing.mailpro.admin.service.SmsHomeRecommendProductService;
+import com.haojing.mailpro.mbg.mapper.SmsHomeRecommendProductMapper;
+import com.haojing.mailpro.mbg.model.SmsHomeRecommendProduct;
+import com.haojing.mailpro.mbg.model.SmsHomeRecommendProductExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * @author jiange
+ * @date 2020/6/24 10:31
+ */
+@Service
+public class SmsHomeRecommendProductServiceImpl implements SmsHomeRecommendProductService {
+    @Autowired
+    private SmsHomeRecommendProductMapper recommendProductMapper;
+    @Override
+    public int create(List<SmsHomeRecommendProduct> homeRecommendProductList) {
+        for (SmsHomeRecommendProduct recommendProduct : homeRecommendProductList) {
+            recommendProduct.setRecommendStatus(1);
+            recommendProduct.setSort(0);
+            recommendProductMapper.insert(recommendProduct);
+        }
+        return homeRecommendProductList.size();
+    }
+
+    @Override
+    public int updateSort(Long id, Integer sort) {
+        SmsHomeRecommendProduct recommendProduct = new SmsHomeRecommendProduct();
+        recommendProduct.setId(id);
+        recommendProduct.setSort(sort);
+        return recommendProductMapper.updateByPrimaryKeySelective(recommendProduct);
+    }
+
+    @Override
+    public int delete(List<Long> ids) {
+        SmsHomeRecommendProductExample example = new SmsHomeRecommendProductExample();
+        example.createCriteria().andIdIn(ids);
+        return recommendProductMapper.deleteByExample(example);
+    }
+
+    @Override
+    public int updateRecommendStatus(List<Long> ids, Integer recommendStatus) {
+        SmsHomeRecommendProductExample example = new SmsHomeRecommendProductExample();
+        example.createCriteria().andIdIn(ids);
+        SmsHomeRecommendProduct record = new SmsHomeRecommendProduct();
+        record.setRecommendStatus(recommendStatus);
+        return recommendProductMapper.updateByExampleSelective(record,example);
+    }
+
+    @Override
+    public List<SmsHomeRecommendProduct> list(String productName, Integer recommendStatus, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum,pageSize);
+        SmsHomeRecommendProductExample example = new SmsHomeRecommendProductExample();
+        SmsHomeRecommendProductExample.Criteria criteria = example.createCriteria();
+        if(!StringUtils.isEmpty(productName)){
+            criteria.andProductNameLike("%"+productName+"%");
+        }
+        if(recommendStatus!=null){
+            criteria.andRecommendStatusEqualTo(recommendStatus);
+        }
+        example.setOrderByClause("sort desc");
+        return recommendProductMapper.selectByExample(example);
+    }
+}
