22794abf7b481485e8290294f6fdd0da89ab2058
黎立健 <wb-llj614947@alibaba-inc.com>
2020-06-24 10:07:13 +0800

    首页品牌管理定义

---
+++ mailpro-admin/src/main/java/com/haojing/mailpro/admin/controller/sms/SmsHomeBrandController.java
@@ -0,0 +1,16 @@
+package com.haojing.mailpro.admin.controller.sms;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 首页品牌管理
+ * @author jiange
+ * @date 2020/6/24 10:06
+ */
+@RestController
+@Api(tags = "SmsHomeBrandController", description = "首页品牌管理")
+@RequestMapping("/home/brand")
+public class SmsHomeBrandController {
+}
